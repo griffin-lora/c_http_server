d65d8b67a6e1cfc8b34256a61000e8a3592b76cf
griffin-lora <griffin-lora@github.com>
2024-05-22 21:16:55 -0400

    update list

--- src/list.c
+++ src/list.c
@@ -2,15 +2,15 @@
 #include <stdlib.h>
 #include <string.h>
 
-void push_elem_to_list(list_t* list, size_t num_elem_bytes, const void* elem) {
-    list_node_t* node = malloc(sizeof(*node) + num_elem_bytes);
+void push_node_to_list(list_t* list, size_t num_data_bytes, const void* data) {
+    list_node_t* node = malloc(sizeof(*node) + num_data_bytes);
+    memcpy(node->data, data, num_data_bytes);
 
     if (list->head == NULL) {
-        *node = (list_node_t) {
+        node->header = (list_node_header_t) {
             .prev = NULL,
             .next = NULL
         };
-        memcpy(node->data, elem, num_elem_bytes);
 
         *list = (list_t) {
             .head = node,
@@ -19,28 +19,28 @@ void push_elem_to_list(list_t* list, size_t num_elem_bytes, const void* elem) {
         return;
     }
 
-    *node = (list_node_t) {
+    node->header = (list_node_header_t) {
         .prev = list->tail,
         .next = NULL
     };
-    memcpy(node->data, elem, num_elem_bytes);
+    memcpy(node->data, data, num_data_bytes);
     
-    list->tail->next = node;
+    list->tail->header.next = node;
 
     list->tail = node;
 }
 
-void remove_elem_from_list(list_t* list, const list_node_t* node) {
-    if (node->prev) {
-        node->prev->next = node->next;
+void remove_node_from_list(list_t* list, const list_node_t* node) {
+    if (node->header.prev) {
+        node->header.prev->header.next = node->header.next;
     } else {
-        list->head = node->next;
+        list->head = node->header.next;
     }
     
-    if (node->next) {
-        node->next->prev = node->prev;
+    if (node->header.next) {
+        node->header.next->header.prev = node->header.prev;
     } else {
-        list->tail = node->prev;
+        list->tail = node->header.prev;
     }
 
     free((void*) node);
--- src/list.h
+++ src/list.h
@@ -3,9 +3,13 @@
 
 typedef struct list_node list_node_t;
 
-struct list_node {
+typedef struct {
     list_node_t* prev;
     list_node_t* next;
+} list_node_header_t;
+
+struct list_node {
+    list_node_header_t header;
     uint8_t data[];
 };
 
@@ -14,5 +18,5 @@ typedef struct {
     list_node_t* tail;
 } list_t;
 
-void push_elem_to_list(list_t* list, size_t num_elem_bytes, const void* elem);
-void remove_elem_from_list(list_t* list, const list_node_t* node);
+void push_node_to_list(list_t* list, size_t num_data_bytes, const void* data);
+void remove_node_from_list(list_t* list, const list_node_t* node);
